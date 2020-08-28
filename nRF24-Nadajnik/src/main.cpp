87017495ce47507f4b1162c4a29763281bf18234
Printo3DJob <grawires@gmail.com>
2020-08-28 17:00:35 +0200

    gyt

--- nRF24-Nadajnik/src/main.cpp
+++ nRF24-Nadajnik/src/main.cpp
@@ -6,24 +6,25 @@
 * 
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
-/*
+
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
 // BME280 LIB
-//#define TINY_BME280_SPI
-//#include "TinyBME280.h"
-//tiny::BME280 bme1; //Uses I2C address 0x76 (jumper closed)
-//float bme_data;
+#define TINY_BME280_SPI
+#include "TinyBME280.h"
+tiny::BME280 bme1; //Uses I2C address 0x76 (jumper closed)
+float bme_data;
 
 RF24 radio(9, 10); // CE, CSN
-const byte address[6] = "00001";
-int data = 100;
+const byte address[17] = "1100110011001100";
+int data = 0;
 
 void setup() {
-  //bme1.beginSPI(8);
-  Serial.begin(9600);
+  bme1.beginSPI(8);
+  Serial.begin(115200);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_LOW);
@@ -31,8 +32,9 @@ void setup() {
 }
 
 void loop() {
-  //bme_data = bme1.readFixedPressure();  // Odczyt z czujnika bme
-  Serial.println(data);
+  bme_data = bme1.readFixedPressure();  // Odczyt z czujnika bme
+  Serial.print("BME nadano:");
+  Serial.println(bme_data);
   //bme1.end();
 
   const char text[] = "0101010101";
@@ -41,24 +43,106 @@ void loop() {
   //radio.openWritingPipe(address);
   //radio.setPALevel(RF24_PA_LOW);
   //radio.stopListening();
-  radio.write(&data, sizeof(data));
+  radio.write(&bme_data, sizeof(bme_data));
   delay(1000);
-}*/
+}
 
+
+/*
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
-const byte address[13] = "111000111000";
+const byte address[17] = "1100110011001100";
 void setup() {
+  pinMode(10,OUTPUT);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_LOW);
   radio.stopListening();
 }
-int data=100;
+int data=10;
 void loop() {
-  data++;
   radio.write(&data, sizeof(data));
-  delay(1);
+  delay(2000);
+}*/
+
+/*
+// SimpleTx - the master or the transmitter
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
+#define CE_PIN   9
+#define CSN_PIN 10
+
+const byte slaveAddress[5] = {'R','x','A','A','A'};
+
+
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+
+char dataToSend[10] = "Message 0";
+char txNum = '0';
+
+
+unsigned long currentMillis;
+unsigned long prevMillis;
+unsigned long txIntervalMillis = 1000; // send once per second
+
+
+void setup() {
+
+    Serial.begin(115200);
+
+    Serial.println("SimpleTx Starting");
+
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    radio.setRetries(3,5); // delay, count
+    radio.openWritingPipe(slaveAddress);
 }
+//================
+
+void updateMessage() {
+        // so you can see that new data is being sent
+    txNum += 1;
+    if (txNum > '9') {
+        txNum = '0';
+    }
+    dataToSend[8] = txNum;
+}
+
+//====================
+void send() {
+
+    bool rslt;
+    rslt = radio.write( &dataToSend, sizeof(dataToSend) );
+        // Always use sizeof() as it gives the size as the number of bytes.
+        // For example if dataToSend was an int sizeof() would correctly return 2
+
+    Serial.print("Data Sent ");
+    Serial.print(dataToSend);
+    if (rslt) {
+        Serial.println("  Acknowledge received");
+        updateMessage();
+    }
+    else {
+        Serial.println("  Tx failed");
+    }
+}
+
+void loop() {
+    currentMillis = millis();
+    if (currentMillis - prevMillis >= txIntervalMillis) {
+        send();
+        prevMillis = millis();
+    }
+}
+
+//====================
+
+
+
+*/
