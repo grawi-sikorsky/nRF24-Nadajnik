b8decdd66e6a79af3d9828f9755751c2ad71aa94
Printo3DJob <grawires@gmail.com>
2020-09-14 14:18:41 +0200

    a

--- nRF24-Nadajnik/src/main.cpp
+++ nRF24-Nadajnik/src/main.cpp
@@ -1,6 +1,11 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <time.h>
+#include <digitalWriteFast.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
 
 // BME280 LIB
 #define TINY_BME280_SPI
@@ -13,6 +18,61 @@ const byte address[17] = "1100110011001100";
 int data = 0;
 
 void setup() {
+  clock_prescale_set(clock_div_1);
+  
+  // wylacz WDT
+  MCUSR= 0 ;
+  WDTCSR |= _BV(WDCE) | _BV(WDE);
+  WDTCSR = 0;
+
+  ADCSRA &= ~(1 << 7); // TURN OFF ADC CONVERTER
+  power_adc_disable(); // ADC converter
+  //power_spi_disable(); // SPI
+  power_usart0_disable();// Serial (USART)
+  //power_timer0_disable();// TIMER 0 SLEEP WDT ...
+  power_timer1_disable();// Timer 1 - I2C...
+  power_timer2_disable();// Timer 2
+
+  PORTD &= ~(1 << PD0);   // LOW pin0 CMT2110
+
+  for (byte i = 0; i <= A5; i++)
+  {
+    pinModeFast(i, OUTPUT);    // changed as per below
+    digitalWriteFast(i, LOW);  //     ditto
+  }
+
+  //pinModeFast(LED_PIN,OUTPUT);
+  //pinModeFast(SPEAKER_PIN,OUTPUT);
+  //pinModeFast(TRANSMISION_PIN,OUTPUT);
+  //pinModeFast(USER_SWITCH,INPUT);
+  //digitalWriteFast(LED_PIN, LOW);  // LED OFF
+  //digitalWriteFast(SPEAKER_PIN, LOW);    // SPK
+  //digitalWriteFast(TRANSMISION_PIN, LOW);    // RF433
+
+  pinModeFast(SS,OUTPUT);
+  pinModeFast(MOSI,OUTPUT);
+  pinModeFast(MISO,OUTPUT);
+  pinModeFast(SCK,OUTPUT);
+  digitalWriteFast(SS,HIGH);
+  digitalWriteFast(MOSI,HIGH);
+  digitalWriteFast(MISO,HIGH);
+  digitalWriteFast(SCK,HIGH);
+
+  power_timer1_enable();  // Timer 1 - I2C...        
+  //readValues();               // pierwsze pobranie wartosci - populacja zmiennych
+  //prev_press = press_odczyt; // jednorazowe na poczatku w setup
+  //setupTimer1();              // Ustawia timer1
+  power_timer1_disable(); // Timer 1 - I2C...
+
+  //makeMsg();                  // Przygotowuje ramke danych
+
+  //last_blink = millis();
+
+  //startup = false;
+  //was_whistled = false;
+  //uc_state = UC_GO_SLEEP; // default uC state
+
+
   bme1.beginSPI(8);
   Serial.begin(115200);
   radio.begin();
